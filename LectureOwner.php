fc59c1b4005f94b6717e77f23c10feb3d11f626c
BobbyTheCreator <temochkaykvlv@gmail.com>
2022-06-10 15:46:01 +0300

    Require php version and add lecture owner dto

---
+++ LectureOwner.php
@@ -0,0 +1,17 @@
+<?php
+
+class LectureOwner
+{
+    private int $id;
+
+    public function __construct(
+        int $id
+    ) {
+        $this->id = $id;
+    }
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+}
